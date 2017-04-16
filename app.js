d09971dd48b142908c5ee5a8d9cde089c2a181f7
Sergei Panfilov <s.panfilov@maximatelecom.ru>
2017-04-16 15:12:46 +0300

    initial setup

---
+++ app.js
@@ -0,0 +1,11 @@
+// Libs
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Components
+import Component from './components/Component';
+
+// CSS
+import './css/style.css';
+
+ReactDOM.render(<Component />, document.getElementById('container'));
